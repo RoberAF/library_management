1365c22453cbfd572b099d90d86b3e6bbad55dee
RoberAF <rober@PRACTICAS2.ctgalega.local>
2024-10-01 16:59:04 +0200

    Primer commit

---
+++ library_management/library_management/doctype/library_settings/test_library_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Roberto Alvarez and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibrarySettings(FrappeTestCase):
+	pass
